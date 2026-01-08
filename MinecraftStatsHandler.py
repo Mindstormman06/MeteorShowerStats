eae295e52368469622369df4a9ae0a40b98f0074
cyberschooled <alex.green@itas.ca>
2026-01-08 08:59:37 -0800

    added option to config.json to skip offline accounts on a server (keep enabled if you have carpet players)

--- MinecraftStatsHandler.py
+++ MinecraftStatsHandler.py
@@ -68,7 +68,10 @@ class MinecraftStatsHandler:
             elif response.status_code == 404:
                 return "Error: UUID not found. Check if the UUID is correct."
             elif response.status_code == 204:
-                if os.getenv("OFFLINE_SUPPORTED", True):
+                with open('StatCrafterConfig.json') as config_file:
+                    config_data = json.load(config_file)
+                    
+                if config_data.get("offline_supported", True):
                     #"UUID not found, possible offline mode enabled"
                     data = {
                         "name" : "Unknown account (offline)",
